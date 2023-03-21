5735502f6564889fed0556d16c4310ad2e46a35b
Mario Bello <mayito.eb@gmail.com>
2023-03-20 22:25:20 -0400

    Added Prime Ng File Upload component

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Prime Ng
+import { FileUploadModule } from 'primeng/fileupload';
 
 import { AppComponent } from './app.component';
 
@@ -9,7 +13,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    HttpClientModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FileUploadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
