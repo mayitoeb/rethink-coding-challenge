f6b11df910c9418271b51ecca60be7d5ff99a30f
Mario Bello <mayito.eb@gmail.com>
2023-03-22 10:34:18 -0400

    Imported modules

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -5,20 +5,32 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Prime Ng
 import { FileUploadModule } from 'primeng/fileupload';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { CardModule } from 'primeng/card';
 
 import { AppComponent } from './app.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { PatientListComponent } from './patients/patient-list/patients-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploadComponent,
+    PatientListComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
-    FileUploadModule
+    TableModule,
+    FileUploadModule,
+    ToastModule,
+    CardModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
