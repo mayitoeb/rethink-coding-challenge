d31b235346c6e1a1ad080293993b7c6c95b2002b
Mario Bello <mayito.eb@gmail.com>
2023-03-23 00:23:47 -0400

    Removed read repo

--- webapi/Core/Interfaces/IPatientReadRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using webapi.Core.Domain;
-
-namespace webapi.Core.Interfaces
-{
-    public interface IPatientReadRepository
-    {
-        public Task<IEnumerable<Patient>> GetAllAsync();
-    }
-}
