689ba26646cfc7ffb73919557085d1350904d86d
Mario Bello <mayito.eb@gmail.com>
2023-03-23 23:25:00 -0400

    Added model validation

--- webapi/Core/Commands/UpdatePatientCommand.cs
+++ webapi/Core/Commands/UpdatePatientCommand.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 
 namespace webapi.Core.Commands
 {
@@ -8,6 +9,21 @@ namespace webapi.Core.Commands
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public DateTime Birthday { get; set; }
-        public char Gender { get; set; }
+        public string Gender { get; set; } = string.Empty;
+    }
+
+    public class UpdatePatientCommandValidator : AbstractValidator<UpdatePatientCommand>
+    {
+        public UpdatePatientCommandValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+
+            RuleFor(x => x.LastName).NotEmpty();
+
+            RuleFor(x => x.Birthday).LessThan(DateTime.Now);
+
+            RuleFor(x => x.Gender).NotEmpty();
+            RuleFor(x => x.Gender).MaximumLength(1);
+        }
     }
 }
