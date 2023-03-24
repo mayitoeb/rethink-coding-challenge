a00f0b1e0f0ed1e5e1bd82a947a1ff3c20279317
Mario Bello <mayito.eb@gmail.com>
2023-03-24 08:51:56 -0400

    Add error handler for csv upload

--- angularapp/src/app/file-upload/file-upload.component.ts
+++ angularapp/src/app/file-upload/file-upload.component.ts
@@ -19,4 +19,8 @@ export class FileUploadComponent {
     this.uploadedFileEvent.emit(null)
   }
 
+  onError(event: any) {
+    this.messageService.add({ severity: 'error', summary: 'Error uploading the file.', detail: '' });
+  }
+
 }
